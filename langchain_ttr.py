7475fd228dfc83b21351acac0245146df63b8839
SEYEON OH <seyeondoh@gmail.com>
2024-03-13 11:24:17 +0900

    langchain first

---
+++ langchain_ttr.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+openai_api_key=os.getenv("OPENAI_API_KEY")
+
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(openai_api_key=openai_api_key)
+
+output = llm.invoke("2024년 청년 지원 정책에 대하여 알려줘")
+print(output)
+from langchain_core.prompts import ChatPromptTemplate
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "너는 청년을 행복하게 하기 위한 정부정책 안내 컨설턴트야"),
+    ("user", "{input}")
+])
+
+chain = prompt | llm 
+chain.invoke({"input": "2024년 청년 지원 정책에 대해 알려줘"})
